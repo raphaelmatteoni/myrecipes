a4296fc7172f067a57a53bd19b9468cc33949ae7
Raphael Matteoni <raphaelmatteoni@gmail.com>
2018-03-02 15:47:23 -0300

    Add facebook omniauth to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302170706) do
+ActiveRecord::Schema.define(version: 20180302172520) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20180302170706) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
